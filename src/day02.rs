df8d470b55ce1087a6e4060d69bcb6d288e4ab32
tsatke <tim.satke@gmail.com>
2024-12-03 18:12:47 +0100

    add day2 part1 test

--- src/day02.rs
+++ src/day02.rs
@@ -15,45 +15,41 @@ enum MonotonicDirection {
 }
 
 pub fn part1() -> usize {
-    INPUT
-        .lines()
-        .filter(|line| {
-            let res = line
-                .split_whitespace()
-                .map(u8::from_str_fast)
-                .fold_while(MonotonicDirection::None, |acc, x| match acc {
-                    MonotonicDirection::Broken => unreachable!(),
-                    MonotonicDirection::None => Continue(MonotonicDirection::First(x)),
-                    MonotonicDirection::First(old) => {
-                        if x > old && x - old <= 3 {
-                            Continue(MonotonicDirection::Increasing(x))
-                        } else if x < old && old - x <= 3 {
-                            Continue(MonotonicDirection::Decreasing(x))
-                        } else {
-                            Done(MonotonicDirection::Broken)
-                        }
-                    }
-                    MonotonicDirection::Increasing(old) => {
-                        if x > old && x - old <= 3 {
-                            Continue(MonotonicDirection::Increasing(x))
-                        } else {
-                            Done(MonotonicDirection::Broken)
-                        }
-                    }
-                    MonotonicDirection::Decreasing(old) => {
-                        if x < old && old - x <= 3 {
-                            Continue(MonotonicDirection::Decreasing(x))
-                        } else {
-                            Done(MonotonicDirection::Broken)
-                        }
-                    }
-                })
-                .into_inner()
-                != MonotonicDirection::Broken;
-            // println!("{line}: {res}");
-            res
+    INPUT.lines().filter(is_line_valid_pt1).count()
+}
+
+fn is_line_valid_pt1(line: &&str) -> bool {
+    line.split_whitespace()
+        .map(u8::from_str_fast)
+        .fold_while(MonotonicDirection::None, |acc, x| match acc {
+            MonotonicDirection::Broken => unreachable!(),
+            MonotonicDirection::None => Continue(MonotonicDirection::First(x)),
+            MonotonicDirection::First(old) => {
+                if x > old && x - old <= 3 {
+                    Continue(MonotonicDirection::Increasing(x))
+                } else if x < old && old - x <= 3 {
+                    Continue(MonotonicDirection::Decreasing(x))
+                } else {
+                    Done(MonotonicDirection::Broken)
+                }
+            }
+            MonotonicDirection::Increasing(old) => {
+                if x > old && x - old <= 3 {
+                    Continue(MonotonicDirection::Increasing(x))
+                } else {
+                    Done(MonotonicDirection::Broken)
+                }
+            }
+            MonotonicDirection::Decreasing(old) => {
+                if x < old && old - x <= 3 {
+                    Continue(MonotonicDirection::Decreasing(x))
+                } else {
+                    Done(MonotonicDirection::Broken)
+                }
+            }
         })
-        .count()
+        .into_inner()
+        != MonotonicDirection::Broken
 }
 
 pub fn part2() -> usize {
@@ -67,7 +63,7 @@ mod tests {
 
     #[test]
     fn test_results() {
-        assert_eq!(part1(), 0);
+        assert_eq!(part1(), 483);
         assert_eq!(part2(), 0);
     }
 }
