10f0bac22dda47a2381f5554695bdd400b0a2262
tsatke <tim.satke@gmail.com>
2024-12-03 19:20:45 +0100

    group all benchmark functions for a better overview

--- benches/bench.rs
+++ benches/bench.rs
@@ -2,12 +2,12 @@ use aoc2024::{day01, day02};
 use criterion::{criterion_group, criterion_main, Criterion};
 
 fn benchmark(c: &mut Criterion) {
-    c.benchmark_group("Day 01")
-        .bench_function("Part 1", |b| b.iter(day01::part1))
-        .bench_function("Part 2", |b| b.iter(day01::part2));
-    c.benchmark_group("Day 02")
-        .bench_function("Part 1", |b| b.iter(day02::part1))
-        .bench_function("Part 2", |b| b.iter(day02::part2));
+    let mut g = c.benchmark_group("AoC 2024");
+    g.bench_function("Day 1 Part 1", |b| b.iter(day01::part1));
+    g.bench_function("Day 1 Part 2", |b| b.iter(day01::part2));
+    g.bench_function("Day 2 Part 1", |b| b.iter(day02::part1));
+    g.bench_function("Day 2 Part 2", |b| b.iter(day02::part2));
+    g.finish();
 }
 
 criterion_group!(benches, benchmark);
