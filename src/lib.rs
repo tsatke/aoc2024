84aea5484cf431de9e041d633dc73bf0d679612f
tsatke <tim.satke@gmail.com>
2024-12-03 14:44:13 +0100

    remove unnecessary bench stuff

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,24 @@
+#![feature(array_chunks)]
 #![feature(custom_test_frameworks)]
-#![feature(test)]
-#![allow(soft_unstable)]
+#![feature(portable_simd)]
+
+use std::ops::Sub;
+use std::simd::num::SimdInt;
 
 const INPUT: &str = include_str!("../inputs/input_day1.txt");
 
-pub fn solution_01_1() -> usize {
-    const LINES: usize = 1000;
+pub fn solution_01_1() -> u64 {
+    const LINES: usize = (1000 + 63) & !64;
 
-    let mut left = [0_usize; LINES];
-    let mut right = [0_usize; LINES];
+    let mut left = [0_i64; LINES];
+    let mut right = [0_i64; LINES];
     INPUT
         .lines()
         .map(|line| (&line[0..5], &line[8..]))
         .enumerate()
         .for_each(|(i, (a, b))| {
-            left[i] = a.parse::<usize>().unwrap();
-            right[i] = b.parse::<usize>().unwrap();
+            left[i] = a.parse::<i64>().unwrap();
+            right[i] = b.parse::<i64>().unwrap();
         });
 
     left.sort_unstable();
@@ -34,15 +37,8 @@ pub fn solution_01_2() -> usize {
 mod tests {
     use super::*;
 
-    extern crate test;
-
     #[test]
     fn test_part1() {
         assert_eq!(solution_01_1(), 2066446);
-    }
-
-    #[bench]
-    fn bench_part1(b: &mut test::Bencher) {
-        b.iter(|| solution_01_1());
     }
 }
