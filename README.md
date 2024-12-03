524f38193e0a1f7ec5d9c444b7ee547ac4639eed
tsatke <tim.satke@gmail.com>
2024-12-03 18:10:34 +0100

    add day2 benchmarks

---
+++ README.md
@@ -0,0 +1,13 @@
+# Advent of Code 2024
+
+Rust. Performance.
+
+## Benchmarks
+
+Criterion, MacBook Pro M1 Max 2021.
+
+| Day | Part | Time |
+|-----|------|------|
+| 1   | 1    | 22µs |
+|     | 2    | 14µs |
+| 2   | 1    | 40µs |
--- benches/bench.rs
+++ benches/bench.rs
@@ -1,10 +1,13 @@
-use aoc2024::day01::{part1, part2};
+use aoc2024::{day01, day02};
 use criterion::{criterion_group, criterion_main, Criterion};
 
 fn benchmark(c: &mut Criterion) {
     c.benchmark_group("Day 01")
-        .bench_function("Part 1", |b| b.iter(part1))
-        .bench_function("Part 2", |b| b.iter(part2));
+        .bench_function("Part 1", |b| b.iter(day01::part1))
+        .bench_function("Part 2", |b| b.iter(day01::part2));
+    c.benchmark_group("Day 02")
+        .bench_function("Part 1", |b| b.iter(day02::part1))
+        .bench_function("Part 2", |b| b.iter(day02::part2));
 }
 
 criterion_group!(benches, benchmark);
