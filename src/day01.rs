e0aac799522b6d69ec063ba03c639fafef23e6d0
tsatke <tim.satke@gmail.com>
2024-12-03 16:54:56 +0100

    solve part 2

---
+++ src/day01.rs
@@ -0,0 +1,55 @@
+use crate::FromStrFast;
+
+const LINES: usize = 1000;
+const INPUT: &str = include_str!("../inputs/input_day1.txt");
+
+fn populate_columns(left: &mut [i32; 1000], right: &mut [i32; 1000]) {
+    INPUT
+        .lines()
+        .map(|line| {
+            (
+                u32::from_str_fast(&line[0..5]),
+                u32::from_str_fast(&line[8..]),
+            )
+        })
+        .enumerate()
+        .for_each(|(i, (a, b))| {
+            left[i] = a as i32;
+            right[i] = b as i32;
+        });
+}
+
+pub fn solution_01_1() -> u32 {
+    let mut left = [0_i32; LINES];
+    let mut right = [0_i32; LINES];
+    populate_columns(&mut left, &mut right);
+
+    left.sort_unstable();
+    right.sort_unstable();
+
+    left.into_iter()
+        .zip(right.into_iter())
+        .fold(0, |acc, (a, b)| acc + a.abs_diff(b))
+}
+
+pub fn solution_01_2() -> i64 {
+    let mut left = [0_i32; LINES];
+    let mut right = [0_i32; LINES];
+    populate_columns(&mut left, &mut right);
+
+    let mut lut = [0_u16; 100000];
+    right.iter().for_each(|&r| lut[r as usize] += 1);
+    left.iter()
+        .fold(0, |acc, &l| acc + ((l as i64) * lut[l as usize] as i64))
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{solution_01_1, solution_01_2};
+
+    #[test]
+    fn test_results() {
+        assert_eq!(solution_01_1(), 2066446);
+        assert_eq!(solution_01_2(), 24931009);
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,37 +1,8 @@
 #![feature(custom_test_frameworks)]
 
-const INPUT: &str = include_str!("../inputs/input_day1.txt");
+pub use day01::*;
 
-pub fn solution_01_1() -> u32 {
-    const LINES: usize = 1000;
-
-    let mut left = [0_i32; LINES];
-    let mut right = [0_i32; LINES];
-    INPUT
-        .lines()
-        .map(|line| {
-            (
-                u32::from_str_fast(&line[0..5]),
-                u32::from_str_fast(&line[8..]),
-            )
-        })
-        .enumerate()
-        .for_each(|(i, (a, b))| {
-            left[i] = a as i32;
-            right[i] = b as i32;
-        });
-
-    left.sort_unstable();
-    right.sort_unstable();
-
-    left.into_iter()
-        .zip(right.into_iter())
-        .fold(0, |acc, (a, b)| acc + a.abs_diff(b))
-}
-
-pub fn solution_01_2() -> usize {
-    0
-}
+mod day01;
 
 macro_rules! fast_uint_parse_impl {
     ($($typ:ty),*) => {
@@ -52,15 +23,10 @@ pub trait FromStrFast {
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+    use crate::FromStrFast;
 
     #[test]
     fn test_fast_uint_parse() {
         assert_eq!(u32::from_str_fast("123"), 123);
-    }
-
-    #[test]
-    fn test_part1() {
-        assert_eq!(solution_01_1(), 2066446);
     }
 }
