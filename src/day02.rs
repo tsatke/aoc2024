31c3aa8be70c69ce083bd874119df8f1921063f0
tsatke <tim.satke@gmail.com>
2024-12-03 18:59:22 +0100

    solve day 2 part 2

--- src/day02.rs
+++ src/day02.rs
@@ -1,69 +1,66 @@
 use crate::FromStrFast;
-use itertools::FoldWhile::{Continue, Done};
 use itertools::Itertools;
 
-// 491 - too high
 const INPUT: &str = include_str!("../inputs/input_day2.txt");
 
-#[derive(Debug, Copy, Clone, Eq, PartialEq)]
-enum MonotonicDirection {
-    Broken,
-    Increasing(u8),
-    Decreasing(u8),
-    First(u8),
-    None,
-}
-
 pub fn part1() -> usize {
-    INPUT.lines().filter(is_line_valid_pt1).count()
+    INPUT.lines().filter(is_line_valid).count()
 }
 
-fn is_line_valid_pt1(line: &&str) -> bool {
-    line.split_whitespace()
-        .map(u8::from_str_fast)
-        .fold_while(MonotonicDirection::None, |acc, x| match acc {
-            MonotonicDirection::Broken => unreachable!(),
-            MonotonicDirection::None => Continue(MonotonicDirection::First(x)),
-            MonotonicDirection::First(old) => {
-                if x > old && x - old <= 3 {
-                    Continue(MonotonicDirection::Increasing(x))
-                } else if x < old && old - x <= 3 {
-                    Continue(MonotonicDirection::Decreasing(x))
-                } else {
-                    Done(MonotonicDirection::Broken)
-                }
-            }
-            MonotonicDirection::Increasing(old) => {
-                if x > old && x - old <= 3 {
-                    Continue(MonotonicDirection::Increasing(x))
-                } else {
-                    Done(MonotonicDirection::Broken)
-                }
-            }
-            MonotonicDirection::Decreasing(old) => {
-                if x < old && old - x <= 3 {
-                    Continue(MonotonicDirection::Decreasing(x))
-                } else {
-                    Done(MonotonicDirection::Broken)
-                }
-            }
-        })
-        .into_inner()
-        != MonotonicDirection::Broken
+fn is_line_valid(line: &&str) -> bool {
+    let mut elems = [0_u8; 8];
+    let mut last = into_array(&mut elems, line.split_whitespace().map(u8::from_str_fast));
+    let elems = &elems[..=last];
+
+    is_slice_valid(elems)
+}
+
+fn is_slice_valid(elems: &[u8]) -> bool {
+    elems
+        .iter()
+        .tuple_windows()
+        .all(|(&a, &b)| (1..=3).contains(&a.abs_diff(b)))
+        && (elems.is_sorted() || elems.iter().rev().is_sorted())
+}
+
+fn into_array(arr: &mut [u8; 8], items: impl Iterator<Item = u8>) -> usize {
+    let mut last = 0;
+    items.enumerate().for_each(|(i, n)| {
+        arr[i] = n;
+        last = i;
+    });
+    last
 }
 
 pub fn part2() -> usize {
-    0
+    INPUT
+        .lines()
+        .filter(|line| {
+            is_line_valid(line) || {
+                (0..line.len()).any(|skip| {
+                    let mut elems = [0_u8; 8];
+                    let mut last = into_array(
+                        &mut elems,
+                        line.split_whitespace()
+                            .map(u8::from_str_fast)
+                            .enumerate()
+                            .filter_map(|(i, n)| if i == skip { None } else { Some(n) }),
+                    );
+                    let elems = &elems[..=last];
+                    is_slice_valid(elems)
+                })
+            }
+        })
+        .count()
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::day01::part2;
     use crate::day02::{part1, part2};
 
     #[test]
     fn test_results() {
         assert_eq!(part1(), 483);
-        assert_eq!(part2(), 0);
+        assert_eq!(part2(), 528);
     }
 }
